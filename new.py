0538dbb0919961ad1a84bc15cc57740abb8c3618
Ayo <olubayo.ayobami21@gmail.com>
2023-05-26 10:04:42 +0000

    continuera

--- new.py
+++ new.py
@@ -1,1 +1,13 @@
-hello world
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# get method using root ('/')
+@app.get("/")
+async def testing():
+    return {'reponse' : 'Salut World'}
+
+#get method using root ('/posts')
+@app.get("/comments")
+def get_comments():
+    return {'data' : 'votre reponse est ici'}
