6cc0d1097d335690a021155bcf2ef3a3a3532d6e
Ayo <olubayo.ayobami21@gmail.com>
2023-05-27 03:52:48 +0000

    27th

--- new.py
+++ new.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# get method using root ('/')
+class Posts(BaseModel):
+    Name: str
+    Job: str
+    
+#get method using root ('/')
 @app.get("/")
 async def testing():
     return {'reponse' : 'Salut World'}
 
-#get method using root ('/posts')
+#get method using root ('/comment')
 @app.get("/comments")
 def get_comments():
     return {'data' : 'votre reponse est ici'}
+
+#post method
+@app.post("/createposts")
+def create_comments(post: Posts):
+    print(post)
+    return {"mon reponse" : "nouvel"}
