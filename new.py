c008984c6d835ace3c3bfbd10fe929ae1e89ad0b
Ayo <olubayo.ayobami21@gmail.com>
2023-06-05 09:57:36 +0000

    ine

--- new.py
+++ new.py
@@ -1,19 +1,27 @@
-from typing import Optional
+#from typing import Optional
 from fastapi import FastAPI
-from fastapi.params import Body
+#from fastapi.params import Body
 from pydantic import BaseModel
-from random import randrange
+#from random import randrange
 
 app = FastAPI()
 
-class Posts(BaseModel):
-    Name: str
-    Job: str
+#class Posts(BaseModel):
+#    Name: str
+ #   Job: str
+  #  published: bool
+   # rating: Optional[int] = None
     
-my_posts = [{
-    "Name" : "Ayo", "Job" : "Software Developer", "Communication" : "Languages spoken", "Native" : "Yoruba", "Studied with" : "English", "Learnt" : "Francais", "id" : 1}, {
-    "Name" : "Bami", "Job" : "Cloud Engineer", "Communication" : "Languages spoken", "Native" : "Yoruba", "Studied with" : "English", "Learnt" : "Spanish", "id" : 2
-}]
+#my_posts = [{
+ #   "Name" : "Ayo", "Job" : "Software Developer", "Native" : "Yoruba", "Learnt" : "Francais", "id" : 1}, {
+  #  "Name" : "Bami", "Job" : "Cloud Engineer", "Native" : "Yoruba", "Learnt" : "Spanish", "id" : 2
+#}]
+
+#def find_post(id):
+ #   for p in my_posts:
+  #      if p["id"] == id:
+   #         return p
+
 #get method using root ('/')
 @app.get("/")
 def testing():
@@ -22,17 +30,19 @@ def testing():
 #get method using root ('/comment')
 @app.get("/posts")
 def get_comments():
-    return {'data' : my_posts}
+    return {'data' : 'my_posts'}
 
 #post method
-@app.post("/posts")
-def create_comments(post: Posts):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"mon reponse" : post_dict}
+@app.post("/")
+def create_comments():
+ #   post_dict = post.dict()
+  #  post_dict['id'] = randrange(0, 1000000)
+   # my_posts.append(post_dict)
+    return {"mon reponse" : "post_dict"}
 
 # getting specific post using id
-@app.get('posts/{id}')
-def get_specific_comment(id):
-    print(id)
+#@app.get('posts/{id}')
+#def get_specific_comment(id : int):
+ #   post = find_post(id)
+  #  print(post)
+   # return {"la detail" : post}
