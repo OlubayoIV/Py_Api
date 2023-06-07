fee3a756457cc26a920239faef4e4c0c094bb027
Ayo <olubayo.ayobami21@gmail.com>
2023-06-07 11:08:32 +0000

    pain

--- new.py
+++ new.py
@@ -1,48 +1,49 @@
-#from typing import Optional
-from fastapi import FastAPI
-#from fastapi.params import Body
+from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
 from pydantic import BaseModel
-#from random import randrange
+from random import randrange
 
 app = FastAPI()
 
-#class Posts(BaseModel):
-#    Name: str
- #   Job: str
-  #  published: bool
-   # rating: Optional[int] = None
+class Post(BaseModel):
+    Name: str
+    Job: str
+    rating: Optional[int] = None
     
-#my_posts = [{
- #   "Name" : "Ayo", "Job" : "Software Developer", "Native" : "Yoruba", "Learnt" : "Francais", "id" : 1}, {
-  #  "Name" : "Bami", "Job" : "Cloud Engineer", "Native" : "Yoruba", "Learnt" : "Spanish", "id" : 2
-#}]
+my_posts = [{
+    "Name" : "Ayo", "Job" : "Software Developer", "Native" : "Yoruba", "Learnt" : "Francais", "id" : 1}, {
+    "Name" : "Bami", "Job" : "Cloud Engineer", "Native" : "Yoruba", "Learnt" : "Spanish", "id" : 2
+}]
 
-#def find_post(id):
- #   for p in my_posts:
-  #      if p["id"] == id:
-   #         return p
+def find_post(id):
+   for p in my_posts:
+      if p["id"] == id:
+        return p
 
 #get method using root ('/')
 @app.get("/")
-def testing():
+def root():
     return {'reponse' : 'Salut World'}
 
 #get method using root ('/comment')
 @app.get("/posts")
 def get_comments():
-    return {'data' : 'my_posts'}
+    return {'data' : my_posts}
 
-#post method
-@app.post("/")
-def create_comments():
- #   post_dict = post.dict()
-  #  post_dict['id'] = randrange(0, 1000000)
-   # my_posts.append(post_dict)
-    return {"mon reponse" : "post_dict"}
+#post method 
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_comments(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"mon reponse" : post_dict}
 
 # getting specific post using id
-#@app.get('posts/{id}')
-#def get_specific_comment(id : int):
- #   post = find_post(id)
-  #  print(post)
-   # return {"la detail" : post}
+@app.get('/posts/{id}')
+def get_specific_comment(id : int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                            detail = f'post with id : {id} was not found')
+    return {"la detail" : post}
