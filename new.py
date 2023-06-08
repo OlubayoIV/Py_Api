7605450c8d050e6b905b05c399ea8a243dc1f079
Ayo <olubayo.ayobami21@gmail.com>
2023-06-08 14:22:36 +0000

    progress

--- new.py
+++ new.py
@@ -6,21 +6,30 @@ from random import randrange
 
 app = FastAPI()
 
+# my schema
 class Post(BaseModel):
     Name: str
     Job: str
     rating: Optional[int] = None
     
+# my hard coded content body
 my_posts = [{
     "Name" : "Ayo", "Job" : "Software Developer", "Native" : "Yoruba", "Learnt" : "Francais", "id" : 1}, {
     "Name" : "Bami", "Job" : "Cloud Engineer", "Native" : "Yoruba", "Learnt" : "Spanish", "id" : 2
 }]
 
+# function for post request
 def find_post(id):
    for p in my_posts:
       if p["id"] == id:
         return p
 
+# function for delete request
+def post_index(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+    
 #get method using root ('/')
 @app.get("/")
 def root():
@@ -47,3 +56,25 @@ def get_specific_comment(id : int):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
                             detail = f'post with id : {id} was not found')
     return {"la detail" : post}
+
+# deleting specific post using id
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_comment(id: int):
+    index = post_index(id)
+    if not index:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'comment with id : {id} is not on database')
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT) 
+
+# updating comment
+@app.put('/posts/{id}')
+def update_comment(id: int, post: Post):
+    index = post_index(id)
+    if not index:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"comment with id : {id} is not on database")
+    post_dict = post.dict()     #to put the post into a dictionary
+    post_dict['id'] = id        #to indicate the id of the post in the dico
+    my_posts[index] = post_dict     #to check and run code if the id matches the index in my_posts
+    return{'data' : post_dict}
